83f2da93fd7081aff9d0fcafe6c8b484249ba5c8
Nour Sofanati <github@nayzac.online>
2022-07-28 17:11:38 +0200

    Fix readonly issue

--- src/pages/organizations/[organizationId].tsx
+++ src/pages/organizations/[organizationId].tsx
@@ -32,35 +32,35 @@ const ViewOrganizationPage: NextPage = () => {
                     <form className="p-4 grid grid-cols-2 gap-4 gap-x-8" onSubmit={handleSubmit(onSubmit)}>
                         <div className="grid gap-2">
                             <label htmlFor="name" className="form-label">Name:</label>
-                            <input type="text" {...register('name')} className="form-input" value={organization.data?.name} />
+                            <input type="text" {...register('name')} className="form-input" />
                         </div>
                         <div className="grid gap-2">
                             <label htmlFor="email" className="form-label">Email:</label>
-                            <input type="text" {...register('email')} className="form-input" value={organization.data?.email} />
+                            <input type="text" {...register('email')} className="form-input" />
                         </div>
                         <div className="grid gap-2">
                             <label htmlFor="phone" className="form-label">Phone:</label>
-                            <input type="text" {...register('phone')} className="form-input" value={organization.data?.phone} />
+                            <input type="text" {...register('phone')} className="form-input" />
                         </div>
                         <div className="grid gap-2">
                             <label htmlFor="address" className="form-label">Address:</label>
-                            <input type="text" {...register('address')} className="form-input" value={organization.data?.address} />
+                            <input type="text" {...register('address')} className="form-input" />
                         </div>
                         <div className="grid gap-2">
                             <label htmlFor="city" className="form-label">City:</label>
-                            <input type="text" {...register('city')} className="form-input" value={organization.data?.city} />
+                            <input type="text" {...register('city')} className="form-input" />
                         </div>
                         <div className="grid gap-2">
                             <label htmlFor="region" className="form-label">Region:</label>
-                            <input type="text" {...register('region')} className="form-input" value={organization.data?.region} />
+                            <input type="text" {...register('region')} className="form-input" />
                         </div>
                         <div className="grid gap-2">
                             <label htmlFor="country" className="form-label">Country:</label>
-                            <input type="text" {...register('country')} className="form-input" value={organization.data?.country} />
+                            <input type="text" {...register('country')} className="form-input" />
                         </div>
                         <div className="grid gap-2">
                             <label htmlFor="postal_code" className="form-label">Postal Code:</label>
-                            <input type="text" {...register('postal_code')} className="form-input" value={organization.data?.postal_code} />
+                            <input type="text" {...register('postal_code')} className="form-input" />
                         </div>
                         <button className=" py-2 px-4 bg-[#2f365f] hover:bg-yellow-500 hover:text-black transition-all text-white " type="submit">Submit</button>
                     </form>
